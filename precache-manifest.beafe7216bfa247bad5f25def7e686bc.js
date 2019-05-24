6e5021c61ad253308723fd64f9e056b1b451fc31
Raihanul Islam Refat <refat0167@gmail.com>
2019-05-24 23:07:47 +0600

    Added files

---
+++ precache-manifest.beafe7216bfa247bad5f25def7e686bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d78bbbf4e389f0df1cfca53e7a4b9c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6535f40e57805cf816a",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4051ecb7e9ea74ff7ca6",
+    "url": "./static/js/2.f8bbee88.chunk.js"
+  },
+  {
+    "revision": "e6535f40e57805cf816a",
+    "url": "./static/js/main.45713979.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
